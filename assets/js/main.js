742fee45b9dba4aa31a0d1c2b85bf923dac972ab
Benjamin Dehli <superelg@gmail.com>
2016-07-17 23:36:58 +0200

    Adjust Gulp tasks

--- assets/js/main.js
+++ assets/js/main.js
@@ -112,3 +112,18 @@ $(document).ready(function () {
         $("body").removeClass("modal-active");
     });
 });
+function updateMasonry() {
+    var container = $('.masonry-row').masonry({
+        itemSelector: '.masonry-item',
+        columnWidth: '.grid-sizer',
+        percentPosition: true
+    });
+    container.imagesLoaded(function () {
+        container.masonry();
+    });
+    container.masonry('reloadItems');
+    container.masonry('layout');
+}
+$(window).on('load', function () {
+    updateMasonry();
+});
