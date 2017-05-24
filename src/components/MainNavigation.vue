4913b9aabb3c6114a7891fe56e71ad63815c999c
benjamindehli <superelg@gmail.com>
2017-05-24 21:59:20 +0200

    Add Vue component for main navigation

---
+++ src/components/MainNavigation.vue
@@ -0,0 +1,66 @@
+<template>
+    <div v-html="htmlContent"></div>
+</template>
+
+<script>
+    import * as quark from 'quark-gui';
+
+    export default {
+        name: 'MainNavigation',
+        data () {
+            return {
+                htmlContent: "",
+                header: {
+                    id: 'main-header',
+                    theme: 'primary',
+                    logo: {
+                        image: {
+                            src: '', //TODO add logo require(''),
+                            alt: 'Benjamin Dehli logo'
+                        },
+                        url: "/"
+                    },
+                    primaryNavigation: {
+                        id: "primary-navigation",
+                        listItems: [
+                            {
+                                name: "Link",
+                                link: "#"
+                            },
+                            {
+                                name: "Dropdown",
+                                link: "#",
+                                dropdownContent: {
+                                    listItems: [
+                                        {
+                                            name: "list item 1",
+                                            link: "#",
+                                            iconClass: "fa fa-music"
+                                        },
+                                        {
+                                            name: "list item 2",
+                                            link: "#",
+                                            iconClass: "fa fa-calendar"
+                                        },
+                                        {
+                                            name: "list item 3",
+                                            link: "#",
+                                            iconClass: "fa fa-group"
+                                        }
+                                    ]
+                                }
+
+                            }
+                        ]
+                    }
+                }
+            }
+        },
+        created: function () {
+            this.htmlContent = quark.Organisms.Global.Header.getModule(this.header);
+        }
+    }
+</script>
+
+
+
