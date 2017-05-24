12f9fc23a5baf769de1bd4d292eb0918f01d6ba3
benjamindehli <superelg@gmail.com>
2017-05-25 01:14:30 +0200

    Adjust styling and content for footer component

--- src/components/MainFooter.vue
+++ src/components/MainFooter.vue
@@ -15,31 +15,28 @@
         methods: {
             createFooterElement: function () {
                 var footerElement = quark.Organisms.Global.Footer.getModule({
-                    logo: {
+                   /* logo: {
                         image: {
                             src: '',//TODO add logo require(''),
                             alt: 'Benjamin Dehli logo'
                         },
                         url: '/'
-                    },
+                    },*/
                     content: `
                     <div itemscope='' itemtype='http://data-vocabulary.org/Person' class='contact-info'>
-          <h3>Contact:</h3>
-          <p><span itemprop='name'>Benjamin Dehli</span></p>
-          <div itemprop='address' itemscope='' itemtype='http://data-vocabulary.org/Address'>
-            <p><span itemprop='street-address'>Margretes veg 15</span></p>
-
-            <p><span itemprop='postal-code'>3804</span> <span itemprop='locality'>Bø i Telemark</span>
-            </p>
-
-            <p><span itemprop='country-name'>Norway</span></p>
-          </div>
-          <p>Phone: +47 92 29 27 19</p>
-          <p>E-mail: superelg@gmail.com</p>
-        </div>
-                        
-			        `
-                });
+                      <h3>Contact:</h3>
+                      <p><span itemprop='name'>Benjamin Dehli</span></p>
+                      <div itemprop='address' itemscope='' itemtype='http://data-vocabulary.org/Address'>
+                        <p><span itemprop='street-address'>Margretes veg 15</span></p>
+                        <p><span itemprop='postal-code'>3804</span> <span itemprop='locality'>Bø i Telemark</span>
+                        </p>
+                        <p><span itemprop='country-name'>Norway</span></p>
+                    </div>
+                    <p>Phone: +47 92 29 27 19</p>
+                    <p>E-mail: superelg@gmail.com</p>
+                </div>
+                `
+            });
                 return footerElement;
             }
         }
@@ -49,6 +46,11 @@
 <style lang="css">
     .contact-info{
         text-align: center;
-        padding: 32px 0;
+    }
+    .contact-info h3{
+        margin: 2px 0;
+    }
+    .contact-info p{
+        margin: 2px 0;
     }
 </style>
