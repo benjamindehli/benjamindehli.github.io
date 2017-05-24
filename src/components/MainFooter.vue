f4d8cecef41147445be0dfde34d0d67d794ea97e
benjamindehli <superelg@gmail.com>
2017-05-24 21:55:49 +0200

    Add Vue component for footer

---
+++ src/components/MainFooter.vue
@@ -0,0 +1,33 @@
+<template>
+    <div v-html="htmlContent"></div>
+</template>
+
+<script>
+    import * as quark from 'quark-gui';
+
+    export default {
+        name: 'MainFooter',
+        data () {
+            return {
+                htmlContent: this.createFooterElement()
+            }
+        },
+        methods: {
+            createFooterElement: function () {
+                var footerElement = quark.Organisms.Global.Footer.getModule({
+                    logo: {
+                        image: {
+                            src: '',//TODO add logo require(''),
+                            alt: 'Benjamin Dehli logo'
+                        },
+                        url: '/'
+                    },
+                    content: `
+                        
+			        `
+                });
+                return footerElement;
+            }
+        }
+    }
+</script>
